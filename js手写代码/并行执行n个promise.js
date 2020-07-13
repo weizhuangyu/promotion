67f3498f607e537727450597727207f9c6764e69
weizhuangyu <weizhuangyu@bilibili.com>
2020-07-13 19:03:26 +0800

    [update]优化并行执行n个promise

--- js手写代码/并行执行n个promise.js
+++ js手写代码/并行执行n个promise.js
@@ -2,22 +2,21 @@
  * 写一个方法，同时最多并行运行n个promise, 多余的会等待前面的执行完后再执行
  */
 
-function runTask(promiseArray, runLength, n) {
+function runTask(promiseArray) {
 	const task = promiseArray.shift()
-	task().then((result) => {
-		console.log(result)
-		runLength--
-		if (promiseArray.length && runLength < n) {
-			runTask(promiseArray, runLength, n)
-		}
-	})
+	if (task) {
+		task().then((result) => {
+			console.log(result)
+			runTask(promiseArray)
+		})
+	}
 }
 
 function run (promiseArray, n) {
 	let runLength = 0
 	while (runLength < n && promiseArray.length) {
 		runLength++
-		runTask(promiseArray, runLength, n)
+		runTask(promiseArray)
 	}
 }
 
